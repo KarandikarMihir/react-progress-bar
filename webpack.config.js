a93deb90ef6adccf5f0d2dd2591e439729a202ea
Mihir Karandikar <karandikar.mihir@outlook.com>
2016-10-20 19:55:41 +0530

    Decompose webpack.config.js (#6)
    * Add HTMLWebpackPlugin and add index.html template
    
    * Setup and test Hello React
    
    * Update README.md
    
    * Configure webpack dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,11 @@
-const path = require('path');
-const loaders = require('./webpack.loaders');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const baseConfiguration = require('./webpack.config.base');
+const devConfiguration = require('./webpack.config.dev');
+const prodConfiguration = require('./webpack.config.prod');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: loaders
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-      inject: true,
-    })
-  ],
-  devServer: {
-    stats: 'errors-only',
-  },
-};
+const customConfiguration = process.env.NODE_ENV === 'production' ? prodConfiguration : devConfiguration;
+
+module.exports = Object.assign(
+  {},
+  baseConfiguration,
+  customConfiguration
+);
