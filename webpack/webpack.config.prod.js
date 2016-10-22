a3de3c871c337390cdd9b3f26ee39365806ebfff
Mihir Karandikar <karandikar.mihir@outlook.com>
2016-10-22 09:51:10 +0530

    Update webpack.config.prod.js

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -23,6 +23,13 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false, // ...but do not show warnings in the console (there is a lot of them)
+      },
+    }),
   ],
 };
